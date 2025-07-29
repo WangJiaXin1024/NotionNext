6b23031f837e7eb42032dd02c97523fff8f610f1
YuanJue <60333635+WangJiaXin1024@users.noreply.github.com>
2025-07-29 15:34:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,8 +4,8 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '23f7c582355f80baa4d0f918857c982e',
-  THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
+    '23f7c582355f800b985ef7b626645ea2',
+  THEME: process.env.NEXT_PUBLIC_THEME || 'typography', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
 
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '愿觉 འཇིགས་མེད་རྡོ་རྗེ་ JigmeDorje ', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '愿成正觉', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://yuanjuebd.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ti.yuanjuebd.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Buddhism', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
